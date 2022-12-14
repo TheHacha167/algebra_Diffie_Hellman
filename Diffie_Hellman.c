f8b1f8439b9458b9dc7937ad0b1353badc030196
Jesus_hna <thehacha167@gmail.com>
2022-12-14 12:03:29 +0100

    enunciado

--- Diffie_Hellman.c
+++ Diffie_Hellman.c
@@ -1,5 +1,8 @@
 /*
-
+Dada una aritmética modular en número primo p=761 y raíz primitiva r=6, 
+implementa un programa que genere un número aleatorio x, calcule X=rx mod p y 
+lo imprima. A continuación, el programa recoge como entrada por consola el número Y 
+que ha generado otro usuario y genera el número K=Yx=rxy.
 */
 #include <stdio.h>
 
